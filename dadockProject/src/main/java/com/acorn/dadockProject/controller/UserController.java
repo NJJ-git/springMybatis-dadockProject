e6a971599d6b983201da23e389a262f28ba79156
ygs <yang40041363@gmail.com>
2022-08-15 21:39:31 +0900

    CRUD 1차 작성

--- dadockProject/src/main/java/com/acorn/dadockProject/controller/UserController.java
+++ dadockProject/src/main/java/com/acorn/dadockProject/controller/UserController.java
@@ -1,20 +1,65 @@
 package com.acorn.dadockProject.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.acorn.dadockProject.dto.UserDto;
+import com.acorn.dadockProject.mapper.UserMapper;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+	@Autowired
+	private UserMapper userMapper;
 	
-	@GetMapping("/profile")
-	public void profile() {
+	@GetMapping("/list/{page}")
+	public String list(@PathVariable int page, Model model) {
+		List<UserDto>userList=(userMapper.selectAll());
+		System.out.println(userList);
+		model.addAttribute(userList);
 		
+		return "/user/list";
 	}
-	@GetMapping("/register")
-	public void register() {
-		
+	@GetMapping("/register.do")
+	public void register() {};
+	
+	@PostMapping("/register.do")
+	public String register(UserDto userDto) {
+		int regist=0;
+		regist=userMapper.insertOne(userDto);
+		if(regist>0) {
+			return "redirect:/";
+		}else {
+			return "redirect:/user/register.do";
+		}
 	}
-
+	@GetMapping("/delete.do/{userId}")
+	public String delete(@PathVariable String userId) {
+		int delete=0;		
+		try {
+			delete=userMapper.deleteOne(userId);				
+		} catch (Exception e) {	e.printStackTrace();}
+	if(delete>0) {
+			return "redirect:/user/list/1";
+	} else {
+			return "redirect:/user/detail/"+userId;
+	}
+  }
 }
+
+
+
+
+
+
+
+
+
+
