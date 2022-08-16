a1a2ee0084516f0cb2edec7b4e325297af06ec7e
NJ <gorun@naver.com>
2022-08-16 15:06:29 +0900

    test

--- dadockProject/src/main/java/com/acorn/dadockProject/controller/BookController.java
+++ dadockProject/src/main/java/com/acorn/dadockProject/controller/BookController.java
@@ -1,26 +1,27 @@
 package com.acorn.dadockProject.controller;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import java.util.ArrayList;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.acorn.dadockProject.service.BookApiCallService;
 
 @Controller
 @RequestMapping("/book")
 public class BookController {
+	@Autowired
+	private BookApiCallService service;
+	
 	@GetMapping("/register")		
 	public void register() {
 		
@@ -41,23 +42,36 @@ public class BookController {
 		
 	}
 	
+	/*
+	 * @GetMapping("/searchList") public ResponseEntity<JSONObject>
+	 * search(@RequestParam String text, Model model) { BookApiCallService
+	 * bookApiCallService = new BookApiCallService(); String apiURL=
+	 * "https://openapi.naver.com/v1/search/book.json?start=1&display=10&query=";
+	 * JSONObject naver_result = new JSONObject(); try { String
+	 * query=URLEncoder.encode(text, "UTF-8"); naver_result =
+	 * bookApiCallService.get(apiURL+query);
+	 * 
+	 * //BookDto book = mapper.readValue(new URL(apiURL+query), BookDto.class);
+	 * //System.out.println(book);
+	 * 
+	 * // result=bookApiCallService.get(
+	 * "https://openapi.naver.com/v1/search/book_adv.xml?d_catg=100&d_titl="+query);
+	 * JSONArray naver_result_arr=(JSONArray)naver_result.get("items"); Long
+	 * total_long=(Long)naver_result.get("total"); // total : long 타입으로 들어옴 int
+	 * total=Math.toIntExact(total_long); // System.out.println(total); }catch
+	 * (Exception e) { e.printStackTrace(); return new
+	 * ResponseEntity<JSONObject>(naver_result, HttpStatus.INTERNAL_SERVER_ERROR); }
+	 * return new ResponseEntity<JSONObject>(naver_result, HttpStatus.OK); }
+	 */
 	@GetMapping("/searchList")
-    public ResponseEntity searchList(@RequestParam String text, Model model) {
-        BookApiCallService bookApiCallService = new BookApiCallService();
-        JSONObject naver_result = new JSONObject();
-        try {
-        	String query=URLEncoder.encode(text, "UTF-8");
-        	naver_result = bookApiCallService.get("https://openapi.naver.com/v1/search/book.json?"
-            		+ "start=1&display=100&query="+query);
-//        	result=bookApiCallService.get("https://openapi.naver.com/v1/search/book_adv.xml?d_catg=100&d_titl="+query);
-            JSONArray naver_result_arr=(JSONArray)naver_result.get("items");
-            Long total_long=(Long)naver_result.get("total"); // total : long 타입으로 들어옴
-            int total=Math.toIntExact(total_long);
-//            System.out.println(total);
-        }catch (Exception e) {
-        	e.printStackTrace();
-            return new ResponseEntity(naver_result, HttpStatus.INTERNAL_SERVER_ERROR);
+	public ModelAndView searchList(@RequestParam(required=false)String keyword, Model model) {
+		ModelAndView mav = new ModelAndView();
+		if(keyword !=null)
+        {
+            mav.addObject("searchList",service.searchBook(keyword,10,1));
         }
-        return new ResponseEntity(naver_result, HttpStatus.OK);
+        mav.setViewName("searchList");
+        return mav;
 	}
+	
 }
