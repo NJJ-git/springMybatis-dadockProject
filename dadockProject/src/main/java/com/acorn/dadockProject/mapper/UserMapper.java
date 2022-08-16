9f09ed0581b1ebf90a0828263fe539deb97b85a2
artistchb <chb4207@naver.com>
2022-08-16 17:55:07 +0900

    금성 현빈 user 수정

---
+++ dadockProject/src/main/java/com/acorn/dadockProject/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.acorn.dadockProject.mapper;
+//com.acorn.dadockProject.mapper.UserMapper
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.acorn.dadockProject.dto.User;
+@Mapper
+public interface UserMapper {
+	List<User> selectPageAll();
+	User selectOne(String userId);
+	User selectPwOne(String userId, String pw);
+	int deleteOne(String userId);
+	int updateOne(User user);
+	int registerOne(User user);
+}
