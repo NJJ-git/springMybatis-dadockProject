2da2c5c1e4d8d48bd9c16f288c6e46fa30865147
moon <moon22run@gmail.com>
2022-08-19 12:05:35 +0900

    수정

--- dadockProject/src/main/java/com/acorn/dadockProject/mapper/ReadBookMapper.java
+++ dadockProject/src/main/java/com/acorn/dadockProject/mapper/ReadBookMapper.java
@@ -10,10 +10,10 @@ import com.acorn.dadockProject.dto.UserDto;
 
 @Mapper
 public interface ReadBookMapper {
-	public List<ReadBook> selectAll();
-	public ReadBook selectOne(@Param(value="readbook") int readbook);
-	public int updateOne(@Param(value ="readBook") ReadBook readBook);
-	public int insertOne(ReadBook readBook);
-	public int deleteOne(String readBook);
-	
-}
+	   public List<ReadBook> selectAll();
+	   public ReadBook selectOne(@Param(value="readbook") int readbook);
+	   public int updateOne(@Param(value ="readBook") ReadBook readBook);
+	   public int insertOne(ReadBook readBook);
+	   public int deleteOne(String readBook);
+	   
+	}
